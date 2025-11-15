7b5b567ec7cb19c8e902980eea499545aac7a072
can-ek <can-ek@github.com>
2025-11-14 19:50:55 -0800

    Adding SQL information for RSS Feed models

---
+++ cmdAddFeed.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	intdb "github.com/can-ek/gator/internal/database"
+	"github.com/google/uuid"
+)
+
+func handleAddFeed(s *state, cmd command) error {
+	if len(cmd.args) < 2 {
+		return fmt.Errorf("addfeed expects name and url arguments")
+	}
+
+	name := cmd.args[0]
+	url := cmd.args[1]
+	ctx := context.Background()
+	user, err := s.db.GetUser(ctx, s.configuration.CurrentUsername)
+	if err != nil {
+		return err
+	}
+
+	currTime := time.Now()
+	params := intdb.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: currTime,
+		UpdatedAt: currTime,
+		Name:      name,
+		Url:       url,
+		UserID:    user.ID,
+	}
+
+	feed, err := s.db.CreateFeed(ctx, params)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(feed)
+	return nil
+}
